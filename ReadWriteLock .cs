1f3058d6fd82ae1dfab8542545c4401000399fb1
astavile <66729739+astavile@users.noreply.github.com>
2020-06-10 14:51:00 +0300

    Update ReadWriteLock .cs

--- ReadWriteLock .cs
+++ ReadWriteLock .cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace NetProtocol.Lockers
+namespace ExtraLockers
 {
     public struct ReadWriteLock : IDisposable
     {
